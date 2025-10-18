e5f9e1c39078998a45623bb43d0fcb16b68e5c9a
Ivan Fotin <xzvtvxs@gmail.com>
2025-10-18 17:49:09 +0300

    V.3.0
    -add 3 lesson

---
+++ js/lesson-3.js
@@ -0,0 +1,134 @@
+// Дано масив чисел [1, 2, 3, 4, 5]. Створіть новий масив, що містить
+// квадрати кожного елементу вхідного масиву.
+// Очікуваний результат: [1, 4, 9, 16, 25].
+
+// const numbers = [1, 2, 3, 4, 5];
+
+// const newArrNum = numbers.map(number => number ** 2);
+
+// console.log(newArrNum);
+// ____________________________________________________________________TASK 3
+// Дано масив об'єктів.
+// Перевірте, чи є хоча б один об'єкт з віком менше 20 років.
+// Очікуваний результат: true.
+
+// const people = [
+//  { name: 'John', age: 27 },
+//  { name: 'Jane', age: 31 },
+//  { name: 'Bob', age: 19 },
+// ];
+
+// const arrRes = people.some(person => person.age < 20);
+
+// console.log(arrRes);
+
+// _____________________________________________________________________TASK 6
+
+// Відсортуйте масив чисел [4, 2, 5, 1, 3]
+// у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
+
+// const numbersArray = [4, 2, 5, 1, 3];
+
+// const sortNumbers = numbersArray.toSorted((a, b) => a - b);
+// console.log(sortNumbers);
+
+// _________________________________________________________________________TASK 7
+
+// Відсортуйте масив рядків ["banana", "orange", "apple", "pear"]
+//  у порядку алфавіту.
+// Очікуваний результат: ["apple", "banana", "orange", "pear"].
+
+// const stringArray = ['banana', 'orange', 'apple', 'pear'];
+
+// const sortetWord = stringArray.toSorted((a, b) => a.localeCompare(b));
+
+// console.log(sortetWord);
+
+// ___________________________________________________________________________TASK 11
+
+ // Розроби клас Calculator, який дозволяє виконувати арифметичні 
+ //  операції над числом за допомогою методів класу, підтримуючи  
+ // ланцюжковий виклик (method chaining).
+ //
+ // Вимоги до класу Calculator
+ // - Метод number(value)
+ // Встановлює початкове значення для наступних обчислень.
+ // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод getResult, Повертає поточний результат усіх операцій.
+ // Не змінює значення, просто повертає його.
+ 
+ // - метод add - Додає value до поточного значення.
+ // Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод substruct - Віднімає value від поточного значення. Повертає this.
+ 
+// - метод divide - Ділить поточне значення на value, якщо value не дорівнює 0.
+// Якщо value === 0, викидає помилку з повідомленням про неможливість ділення.
+// Повертає сам об'єкт (this) для підтримки ланцюжкових викликів.
+ 
+ // - метод multiply -Множить поточне значення на value. Повертає this.
+ 
+ // Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
+ 
+ // Приклад використання:
+ // const calc = new Calculator();
+
+
+// const result = calc
+//   .number(10)   // Встановлюємо початкове значення 10
+//   .add(5)       // Додаємо 5 (10 + 5 = 15)
+//   .subtract(3)  // Віднімаємо 3 (15 - 3 = 12)
+//   .multiply(4)  // Множимо на 4 (12 * 4 = 48)
+//   .divide(2)    // Ділимо на 2 (48 / 2 = 24)
+//   .getResult(); // Отримуємо результат: 24
+
+
+//   console.log(result); // 24
+
+// class Calculator { 
+//     number(value) {
+//         this.value = value;
+//         return this; 
+//     }
+
+//     getResult() {
+//         return this.value;
+//     }
+
+//     add(value) {
+//         this.value += value;
+//         return this;
+//     }
+
+//     subtract(value) {
+//         this.value -= value;
+//         return this;
+//     }
+
+//     divide(value) {
+//         if (value === 0) {
+//             console.log("Нельзя делить на ноль");
+//         } else {
+//             this.value /= value;
+//         }
+//         return this;
+//     }
+
+//     multiply(value) {
+//         this.value *= value;
+//         return this;
+//     }
+// }
+// const calc = new Calculator();
+
+// const result = calc
+//     .number(10)
+//     .add(5)
+//     .subtract(3)
+//     .multiply(4)
+//     .divide(2)
+//     .getResult()
+// console.log(calc);
+// console.log(result);
+
